d6b80ef1d87e2dcb14d3c7466efc237e69b9254a
culturalist <danil.lopatkin@culturas.fi>
2023-08-03 18:25:48 +0700

    Setting up apps structure

--- apps/template/app/(admin)/admin/[[...index]]/page.tsx
+++ apps/template/app/(admin)/admin/[[...index]]/page.tsx
@@ -1,8 +1,19 @@
-import { Studio } from './Studio';
+'use client';
 
-// Set the right `viewport`, `robots` and `referer` meta tags
-export { metadata } from 'next-sanity/studio/metadata';
+import { NextStudio } from 'next-sanity/studio';
 
-export default function StudioPage() {
-    return <Studio />;
+import config from '../../../../sanity.config';
+
+export default function Studio() {
+    //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
+    return <NextStudio config={config} />;
 }
+
+// import { Studio } from './Studio';
+
+// // Set the right `viewport`, `robots` and `referer` meta tags
+// export { metadata } from 'next-sanity/studio/metadata';
+
+// export default function StudioPage() {
+//     return <Studio />;
+// }
