dd76db1630d5e90f2664c8330b3d4d54002d65e8
culturalist <danil.lopatkin@culturas.fi>
2023-08-18 11:39:55 +0600

    Adding header component

--- packages/data/schemas/values/documents.ts
+++ packages/data/schemas/values/documents.ts
@@ -1,7 +1,7 @@
 import { Event, Note, Page, Person, Post, Project } from '../documents';
 
 export type DocumentApp = 'page' | 'person' | 'post' | 'project' | 'event' | 'note';
-export type DocumentSystem = 'theme' | 'label';
+export type DocumentSystem = 'app' | 'theme' | 'label';
 export type DocumentAny = DocumentApp | DocumentSystem;
 
 export type PageDocument = Event | Note | Page | Person | Post | Project;
