667f51fa08dc0e77c1d140e96f5e87b7255ffc2e
culturalist <danil.lopatkin@culturas.fi>
2024-04-09 13:35:52 +0500

    Fixing issue with notes

--- packages/data/schemas/documents/note/note.query.ts
+++ packages/data/schemas/documents/note/note.query.ts
@@ -1,7 +1,7 @@
 import { groq } from "next-sanity";
 import { bodySegment, mediaBlockSegment } from "../../objects";
 
-export const noteQuery = groq`*[_type == 'note' && slug.current == $slug && (dateTime(publishDate) < dateTime(now()) || !defined(publishDate)))][0]{
+export const noteQuery = groq`*[_type == 'note' && slug.current == $slug && (dateTime(publishDate) < dateTime(now()) || !defined(publishDate))][0]{
     ...,
     covers[] {
         ${mediaBlockSegment}
