8c9a021780778ebe08c860a8b8b252e6cf755bea
culturalist <danil.lopatkin@culturas.fi>
2024-08-05 18:36:22 +0500

    Fixing issue with localized sitemaps

--- apps/culturas/app/sitemap.ts
+++ apps/culturas/app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from "next";
-import { SitemapFile, generateSitemap } from "@cs/data";
+import { generateSitemap } from "@cs/data";
 import { appConfig, appDomain, appName, routes } from "@cs/globals";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
--- apps/tieto/app/sitemap.ts
+++ apps/tieto/app/sitemap.ts
@@ -1,6 +1,7 @@
-import { SitemapFile, generateSitemap } from "@cs/data";
+import { MetadataRoute } from "next";
+import { generateSitemap } from "@cs/data";
 import { appConfig, appDomain, appName, routes } from "@cs/globals";
 
-export default async function sitemap(): Promise<SitemapFile[]> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return generateSitemap(appName, appDomain, routes, appConfig.schemas.links);
 }
