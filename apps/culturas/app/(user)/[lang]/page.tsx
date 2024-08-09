bba40c72a0157dbc512ee913051f707bd1618982
culturalist <danil.lopatkin@culturas.fi>
2024-08-09 21:49:01 +0500

    Cleaning up debug code

--- apps/culturas/app/(user)/[lang]/page.tsx
+++ apps/culturas/app/(user)/[lang]/page.tsx
@@ -1,8 +1,8 @@
 import { notFound } from "next/navigation";
-import { appConfig, appDomain, appName, DefaultPageProps, routes } from "@cs/globals";
+import { appName, DefaultPageProps } from "@cs/globals";
 import { clientNext } from "@cs/data/client";
 import { App, homeQuery } from "@cs/data/schemas";
-import { generateSitemap, prepareMetadata } from "@cs/data/utils";
+import { prepareMetadata } from "@cs/data/utils";
 import { Body, Hero } from "@cs/ui";
 import { Metadata } from "next";
 
@@ -12,14 +12,12 @@ export default async function Home({ params: { lang } }: DefaultPageProps) {
     const data: App | null = await clientNext.fetch(homeQuery, { appName });
 
     if (!data) return notFound();
-    const sitemap = await generateSitemap(appName, appDomain, routes, appConfig.schemas.links);
 
     return (
         // <>
         <main>
             <Hero data={data.hero} lang={lang} />
             <Body data={data.body} lang={lang} />
-            <pre>{JSON.stringify(sitemap, null, 4)}</pre>
         </main>
         // </>
     );
